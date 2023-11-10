f9ed1369278a5c1f788562cda6c078cbd55bc65a
CodingMechineer <manuel.blaser@outlook.com>
2023-11-10 11:13:56 -0500

    first draft of read speed

--- 000 - CSO_ArduinoProject/src/01_main_CSO.cpp
+++ 000 - CSO_ArduinoProject/src/01_main_CSO.cpp
@@ -5,8 +5,12 @@
 #include <ISOBUS.h>
 
 char spn_buffer[512];  //Data will be temporarily stored to this buffer before being written to the filechar buffer[512];  //Data will be temporarily stored to this buffer before being written to the file
+char UserInput;
 
-ISOBUSMessage receiveMessage;
+void printMessage(ISOBUSMessage msg, char buffer[]);
+
+ISOBUSMessage receiveMessage1;
+ISOBUSMessage receiveMessage2;
 
 void setup()
 {
@@ -17,24 +21,53 @@ void setup()
   // initialize digital pin 2 and 3 as an output.
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+
+  delay(1000);
+
+  Serial.println("Please choose a menu option.");
+  Serial.println("1 - RPM");
+  Serial.println("2 - Speed");
+
 }
 
 void loop()
 {
-  /* Receive data if available */
-  if (ISOBUS.available()) {
-    receiveMessage = ISOBUS.getMessageISOBUS(NBVS_PGN, NBVS_SPN, spn_buffer);
-    if (receiveMessage.status == 0)
-    {
-      Serial.print(receiveMessage.status);
-      Serial.print(",");
-      Serial.print(receiveMessage.id);
-      Serial.print(",");
-      Serial.print(receiveMessage.pgn);
-      Serial.print(",");
-      Serial.print(receiveMessage.spn_data);
-      Serial.print(",");
-      Serial.println(spn_buffer);
+  while(Serial.available()){
+  UserInput = Serial.read();
+
+    /* Receive data if available */
+    if (ISOBUS.available()){
+      if (UserInput =='1'){
+        receiveMessage1 = ISOBUS.getMessageISOBUS(EEC1_PGN, EngineSpeed_SPN, spn_buffer);
+        printMessage(receiveMessage1, spn_buffer);
+        }
     }
+    if (UserInput == '2'){
+        receiveMessage2 = ISOBUS.getMessageISOBUS(NBVS_PGN, NBVS_SPN, spn_buffer);
+        printMessage(receiveMessage2, spn_buffer);
+    }
+      
   }
+}
+
+
+
+void printMessage(ISOBUSMessage msg, char buffer[])
+{
+  if (msg.status == 0){
+    Serial.print(msg.status);
+    Serial.print(",");
+    Serial.print(msg.id);
+    Serial.print(",");
+    Serial.print(msg.pgn);
+    Serial.print(",");
+    Serial.print(msg.spn_data);
+    Serial.print(",");
+    Serial.println(buffer);
+  }
+
+  if (msg.status == 10){
+    Serial.println("PGN not available");
+  }
+  
 }
