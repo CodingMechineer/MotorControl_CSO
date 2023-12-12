66284dc5ff5565bce2a69dfdf0212d7237b24e80
CodingMechineer <manuel.blaser@outlook.com>
2023-12-12 10:29:48 -0500

    started to apply, not finished

--- 000 - CSO_ArduinoProject/src/main_CSO.cpp
+++ 000 - CSO_ArduinoProject/src/main_CSO.cpp
@@ -48,18 +48,45 @@ void setup()
   pinMode(driverPUL, OUTPUT);
   pinMode(driverDIR, OUTPUT);
   stepper.setMaxSpeed(1000);
-  stepper.setSpeed(500.96);
 
   delay(1000);
 
   Serial.println("Please choose a menu option.");
   Serial.println("1 - RPM");
-  Serial.println("2 - Speed");
-  Serial.println("3 - Speed 2");  
 }
 
 void loop()
 {
+  /* Reading of the ISOBUS from the tractor*/
+
+  // "EngineSpeed" and "WBMspeed" pre-determined by library
+  // if (ISOBUS.available()){
+  //   UserInput = Serial.read();
+
+  //   if (delayTimeExpired(1000) == true){
+  //     Serial.println("ISOBUS available");
+  //   }
+
+    UserInput = Serial.read();
+    receiveMessage = ISOBUS.getMessageISOBUS(EEC1_PGN, EngineSpeed_SPN, spn_buffer);
+    if (UserInput =='1'){
+      printMessage(receiveMessage, spn_buffer);
+      
+      if (receiveMessage.spn_data != 0)
+        sevseg.setNumber(receiveMessage.spn_data, 0);
+    }
+
+    /* Navigation based vehicle speed */
+    receiveMessage = ISOBUS.getMessageISOBUS(NBVehicleSpeed_PGN, NBVehicleSpeed_SPN, spn_buffer);
+    if (receiveMessage.pgn == NBVehicleSpeed_PGN){
+        
+          printMessage(receiveMessage, spn_buffer);
+          Serial.println(float(receiveMessage.spn_data)/floatFact); // transmit speed without float variable
+          sevseg.setNumber(receiveMessage.spn_data, 1);            
+    }
+  // }
+  
+
   /* Calculations to convert the speed signal to required rounds per minute */
   float SpeedSignal = float(getPotiMap(potiSim, 5, 15))/floatFact; // Values "simulate" speed between the values
   float effSpeedMS = mphToMs(SpeedSignal);
@@ -67,26 +94,6 @@ void loop()
   float adjSpeedMS = adjPotiSpeed(effSpeedMS, adjPerc); 
   float reqMotRPM = reqRPM(adjSpeedMS, seedWheelDia);
   float reqPPS = RPMtoPPS(reqMotRPM, pulsePerRev);
-
-  // /* Debug the speed calculations */
-  // int str = 10;
-  // char str_simSpeedSignal[str];
-  // char str_effSpeedMS[str];
-  // char str_adjSpeedMS[str];
-  // char str_reqMotRPM[str];
-  // char str_reqPPS[str];
-
-  // dtostrf(simSpeedSignal, 3, 1, str_simSpeedSignal);
-  // dtostrf(effSpeedMS, 3, 1, str_effSpeedMS);
-  // dtostrf(adjSpeedMS, 3, 1, str_adjSpeedMS);
-  // dtostrf(reqMotRPM, 3, 1, str_reqMotRPM);
-  // dtostrf(reqPPS, 3, 1, str_reqPPS);
-
-  // sprintf(debugMessage, "SpeedMPH: %s mph, SpeedMS: %s m/s, AdjSpeedMS: %s, reqMotRPM: %s, reqPPS: %s",
-  // str_simSpeedSignal, str_effSpeedMS, str_adjSpeedMS, str_reqMotRPM, str_reqPPS);
-  // Serial.println(debugMessage);
-
-
 
   // Serial.println("---------------");
   // Serial.println(simSpeedSignal);
@@ -101,40 +108,8 @@ void loop()
   stepper.setSpeed(reqPPS);
   stepper.runSpeed();
 
-
   /* Updates the display */
   sevseg.refreshDisplay(); // Must run repeatedly
   sevseg.setNumberF(adjPerc, 1);
- 
-  while(Serial.available()){
-  UserInput = Serial.read();
-  // UserInput = 1;
-
-    // "EngineSpeed" and "WBMspeed" pre-determined by library
-    if (ISOBUS.available()){
-
-      if (UserInput =='1'){
-        receiveMessage = ISOBUS.getMessageISOBUS(EEC1_PGN, EngineSpeed_SPN, spn_buffer);
-        printMessage(receiveMessage, spn_buffer);
-        
-        if (receiveMessage.spn_data != 0)
-          sevseg.setNumber(receiveMessage.spn_data, 0);
-      }
-
-      if (UserInput == '2'){
-        int potiVal = getPotiMap(potiAdjSpeed, minPercent, maxPercent);
-        sevseg.setNumberF(float(potiVal)/floatFact, 1);
-      }
-
-      if (UserInput == '3'){
-          receiveMessage = ISOBUS.getMessageISOBUS(NBVehicleSpeed_PGN, NBVehicleSpeed_SPN, spn_buffer);
-          printMessage(receiveMessage, spn_buffer);
-          
-          if(receiveMessage.spn_data != 0){
-            Serial.println(float(receiveMessage.spn_data)/floatFact); // transmit speed without float variable
-            sevseg.setNumber(receiveMessage.spn_data, 1);            
-          }
-      }
-    }  
-  }
 }
+
